ad2217ac8c823293bdab340ddc3777144a143b05
FaridaElOraby <41236592+FaridaElOraby@users.noreply.github.com>
2021-06-24 03:14:20 +0200

    Unity

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css'
-import AdminPage from './Pages/adminPage'
+import React from "react";
+import Unity, { UnityContext } from "react-unity-webgl";
+
+const unityContext = new UnityContext({
+  loaderUrl: "build/Desktop.loader.js",
+  dataUrl: "build/Desktop.data",
+  frameworkUrl: "build/Desktop.framework.js",
+  codeUrl: "build/Desktop.wasm",
+});
+
 function App() {
+  function handleOnClickFullscreen() {
+    unityContext.setFullscreen(true);
+  }
+
   return (
-    <div className='App'>
-      <AdminPage />
+    <div>
+      <Unity unityContext={unityContext} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
