91cfccd168458e46ab2f8a574becd14cd34c7645
Farida ElOraby <faridaeloraby@gmail.com>
2021-06-24 13:39:43 +0200

    Unity Progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
 
 const unityContext = new UnityContext({
@@ -6,6 +6,9 @@ const unityContext = new UnityContext({
   dataUrl: "build/Desktop.data",
   frameworkUrl: "build/Desktop.framework.js",
   codeUrl: "build/Desktop.wasm",
+  productName: "Cognition Game",
+  productVersion: "1.0.0",
+  companyName: "Farida ElOraby",
 });
 
 function App() {
@@ -13,9 +16,32 @@ function App() {
     unityContext.setFullscreen(true);
   }
 
+  const [progression, setProgression] = useState(0);
+
+  useEffect(function () {
+    unityContext.on("progress", function (progression) {
+      setProgression(progression);
+    });
+  }, []);
+
   return (
     <div>
-      <Unity unityContext={unityContext} />
+      <div>
+      <p>Loading {progression * 100} percent...</p>
+      </div>
+      <div>
+      <Unity unityContext={unityContext} 
+        style={{
+          height: "100%",
+          width: 950,
+          border: "2px solid black",
+          background: "grey",
+        }}
+      />
+      </div>
+      <div>
+      <button onClick={handleOnClickFullscreen}>Fullscreen</button>
+      </div>
     </div>
   );
 }
